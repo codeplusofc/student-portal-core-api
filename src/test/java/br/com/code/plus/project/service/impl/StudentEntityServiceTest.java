47b459f2bf787856fb667a6390e5acfb83597de6
GuiRuchell <ruchellguilherme100@gmail.com>
2024-11-19 13:45:59 -0300

    implementing main structure for on the development

---
+++ src/test/java/br/com/code/plus/project/service/impl/StudentEntityServiceTest.java
@@ -0,0 +1,14 @@
+package br.com.code.plus.project.service.impl;
+
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class StudentEntityServiceTest {
+
+    @Mock
+    private StudentService studentService;
+
+    //TODO: Escrever testes para as novos retornos.
+}
