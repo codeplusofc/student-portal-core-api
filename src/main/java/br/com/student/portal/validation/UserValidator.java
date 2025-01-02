ff855be15dfbce0ab10889f83010784c39fc99cb
GuiRuchell <ruchellguilherme100@gmail.com>
2025-01-02 15:15:41 -0300

    implementando melhorias no validator de usuário

--- src/main/java/br/com/student/portal/validation/UserValidator.java
+++ src/main/java/br/com/student/portal/validation/UserValidator.java
@@ -2,13 +2,16 @@ package br.com.student.portal.validation;
 
 import br.com.student.portal.exception.BadRequestException;
 import br.com.student.portal.model.User;
-
 import org.springframework.stereotype.Component;
 
 import static io.micrometer.common.util.StringUtils.isEmpty;
+import static java.util.regex.Pattern.matches;
 
 @Component
 public class UserValidator {
+
+
+    //TODO: adding regex on the one Utils
 
     public static void validateRequiredField(Object fieldValue, String fieldName) {
         if (fieldValue == null || (fieldValue instanceof String && isEmpty((String) fieldValue))) {
@@ -20,6 +23,10 @@ public class UserValidator {
         if (isEmpty(name)) {
             validateRequiredField(name, "Name");
         }
+
+        if (!name.matches("^[a-zA-Z\\s]+$")) {
+            throw new BadRequestException("Name should only contain letters and spaces.");
+        }
     }
 
     public static void validateEmail(String email) {
@@ -27,22 +34,31 @@ public class UserValidator {
             validateRequiredField(email, "Email");
         }
 
+        var emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        if (!matches(emailRegex, email)) {
+            throw new BadRequestException("Invalid email format.");
+        }
+
         if (!email.endsWith("@gmail.com")) {
             throw new BadRequestException("Unrecognized Gmail Account Access Attempt");
         }
-
     }
 
     public static void validatePassword(String password) {
         if (isEmpty(password)) {
             validateRequiredField(password, "Password");
         }
+
+        var passwordRegex = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*(),.?\":{}|<>]).{8,}$";
+        if (!matches(passwordRegex, password)) {
+            throw new BadRequestException("Password must be at least 8 characters long, contain at least one letter, one number, and one special character.");
+        }
     }
-    public static void validateFields(User user){
+
+    public static void validateFields(User user) {
         validateName(user.getName());
         validateEmail(user.getEmail());
         validatePassword(user.getPassword());
-
     }
 
 }
