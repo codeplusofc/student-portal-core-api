5ac5bd1c04a7b599d397c4bbaffb5523b5bfb26a
Cg1985-ux <marcocamara1985@gmail.com>
2025-05-14 21:34:59 -0300

    resolvendo comentarios do pull request

--- src/main/java/br/com/student/portal/entity/Course.java
+++ src/main/java/br/com/student/portal/entity/Course.java
@@ -1,52 +1,20 @@
 package br.com.student.portal.entity;
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
+import java.util.UUID;
 
 @Entity
+@Getter
+@Setter
 public class Course {
+    private UUID id;
     private String name;
     private String description;
     private Date startDate;
     private Date endDate;
     private String status;
 
-    public String getName() { return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
