e026f01131fca27d1006c38b9160eff2f9422a5e
GuiRuchell <ruchellguilherme100@gmail.com>
2025-05-16 12:00:40 -0300

    Solving pull request comments on the feature task

--- src/main/java/br/com/student/portal/entity/Course.java
+++ src/main/java/br/com/student/portal/entity/Course.java
@@ -1,5 +1,8 @@
 package br.com.student.portal.entity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +13,8 @@ import java.util.UUID;
 @Getter
 @Setter
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
     private String description;
