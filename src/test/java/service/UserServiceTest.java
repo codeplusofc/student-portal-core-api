a7f024f89d921ac0b2a756238152d22379177dfd
GuiRuchell <ruchellguilherme100@gmail.com>
2025-07-02 12:11:53 -0300

    test: adding find all test

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -14,6 +14,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
@@ -52,12 +53,41 @@ public class UserServiceTest {
         given(userRepository.save(userEntity)).willReturn(userEntity);
         given(userMapper.userEntityIntoUserResponse(userEntity)).willReturn(userResponse);
 
-
         var result = userService.createUser(userRequest);
 
         assertEquals("1234123123@gmail.com", result.getEmail());
         assertEquals("Pedrin", result.getName());
         assertEquals(uuid,uuid);
+
+    }
+
+    @Test
+    public void mustGetAllUsers(){
+        var userOne = new UserEntity("Gui",
+                "gui@gmail.com", "1234");
+        var userTwo= new UserEntity("Otavio",
+                "otavio@gmail.com", "123456");
+
+        var userOneResponse = new UserResponse();
+        userOneResponse.setName("Gui");
+        userOneResponse.setEmail("gui@gmail.com");
+
+        var userTwoResponse = new UserResponse();
+        userTwoResponse.setName("Otavio");
+        userTwoResponse.setEmail("otavio@gmail.com");
+
+        var users = List.of(userOne, userTwo);
+
+        given(userRepository.findAll()).willReturn(users);
+        given(userMapper.userEntityIntoUserResponse(userOne)).willReturn(userOneResponse);
+        given(userMapper.userEntityIntoUserResponse(userTwo)).willReturn(userTwoResponse);
+
+        var result = userService.getAllUsers();
+
+        assertEquals("Gui", result.get(0).getName());
+        assertEquals("gui@gmail.com", result.get(0).getEmail());
+        assertEquals("Otavio", result.get(1).getName());
+        assertEquals("otavio@gmail.com", result.get(1).getEmail());
 
     }
 }
