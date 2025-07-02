578701d8240606a11a843d2e9dba9afe210466da
OtavioColela <otavioborges123@yahoo.com>
2025-07-02 11:25:19 -0300

    test/testing

--- src/test/java/service/UserServiceTest.java
+++ src/test/java/service/UserServiceTest.java
@@ -1,7 +1,9 @@
 package service;
 
 import br.com.student.portal.dto.user.UserRequest;
+import br.com.student.portal.dto.user.UserResponse;
 import br.com.student.portal.entity.UserEntity;
+import br.com.student.portal.mapper.UserMapper;
 import br.com.student.portal.repository.UserRepository;
 import br.com.student.portal.service.UserService;
 import org.junit.Test;
@@ -12,12 +14,15 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 
+import java.util.UUID;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.BDDMockito.given;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
-
+    @Mock
+    UserMapper userMapper;
     @Mock
     UserRepository userRepository;
 
@@ -26,6 +31,7 @@ public class UserServiceTest {
 
     @Test
     public void mustCreateUser(){
+        var uuid = UUID.randomUUID();
         // CRIANDO USUARIO
         var userRequest = new UserRequest();
         // ALTERANDO OS DADOS DO USUARIO
@@ -37,13 +43,21 @@ public class UserServiceTest {
                 userRequest.getEmail(),
                 userRequest.getPassword());
 
+        var userResponse = new UserResponse();
+        userResponse.setEmail("1234123123@gmail.com");
+        userResponse.setName("Pedrin");
+        userResponse.setId(uuid);
+
+        given(userMapper.userRequestIntoUserEntity(userRequest)).willReturn(userEntity);
         given(userRepository.save(userEntity)).willReturn(userEntity);
+        given(userMapper.userEntityIntoUserResponse(userEntity)).willReturn(userResponse);
+
 
         var result = userService.createUser(userRequest);
 
         assertEquals("1234123123@gmail.com", result.getEmail());
         assertEquals("Pedrin", result.getName());
-
+        assertEquals(uuid,uuid);
 
     }
 }
