6a9e75c4a93b2b062825a881714fd7c8ce5f4207
OtavioColela <otavioborges123@yahoo.com>
2025-08-13 12:11:01 -0300

    tests/improving-tests

--- src/test/java/br/com/student/portal/service/VoteServiceTest.java
+++ src/test/java/br/com/student/portal/service/VoteServiceTest.java
@@ -1,33 +1,39 @@
 package br.com.student.portal.service;
 
 
+import br.com.student.portal.dto.user.UserRequest;
+import br.com.student.portal.dto.user.UserResponse;
+import br.com.student.portal.dto.vote.VoteRequest;
 import br.com.student.portal.dto.vote.VoteResponse;
+import br.com.student.portal.entity.AgendaEntity;
+import br.com.student.portal.entity.UserEntity;
+import br.com.student.portal.entity.VoteEntity;
 import br.com.student.portal.exception.ObjectNotFoundException;
 import br.com.student.portal.mapper.VoteMapper;
 import br.com.student.portal.repository.AgendaRepository;
 import br.com.student.portal.repository.UserRepository;
 import br.com.student.portal.repository.VoteRepository;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import static br.com.student.portal.data.AgendaEntityData.generateAgendaEntity;
 import static br.com.student.portal.data.FixedData.*;
-import static br.com.student.portal.data.UserEntityData.generateUserEntity;
-import static br.com.student.portal.data.VoteEntityData.generateVoteEntity;
-import static br.com.student.portal.data.VoteRequestData.generateVoteRequest;
-import static br.com.student.portal.data.VoteResponseData.generateVoteResponse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 
+
 @RunWith(MockitoJUnitRunner.class)
 public class VoteServiceTest {
+
     @Mock
     private VoteRepository voteRepository;
     @Mock
@@ -38,15 +44,33 @@ public class VoteServiceTest {
     private VoteMapper voteMapper;
     @InjectMocks
     private VoteService voteService;
+    @Mock
+    private UserRequest userRequest;
+    @Mock
+    private UserEntity userEntity;
+    @Mock
+    private UserResponse userResponse;
+    @Mock
+    private VoteRequest voteRequest;
+    @Mock
+    private VoteEntity voteEntity;
+    @Mock
+    private VoteResponse voteResponse;
+    @Mock
+    private AgendaEntity agendaEntity;
+
+    @Before
+    public void setup() {
+        voteRequest = new VoteRequest(true, AGENDA_ID, USER_ID);
+        voteEntity = new VoteEntity(VOTE_ID , AGENDA_ID, USER_ID, true);
+        voteResponse = new VoteResponse(VOTE_ID, true, USER_ID, AGENDA_ID);
+        agendaEntity = new AgendaEntity(AGENDA_ID
+                , LocalDateTime.of(2999, 8, 15, 11, 24, 53)
+                , "Retirada do presidente");
+    }
 
     @Test
     public void mustCreateVote(){
-        var voteRequest = generateVoteRequest();
-        var voteEntity = generateVoteEntity();
-        var agendaEntity = generateAgendaEntity();
-        var userEntity = generateUserEntity();
-        var voteResponse = generateVoteResponse();
-
         given(voteMapper.voteRequestIntoVoteEntity(voteRequest)).willReturn(voteEntity);
         given(userRepository.findById(USER_ID)).willReturn(Optional.of(userEntity));
         given(agendaRepository.findById(AGENDA_ID)).willReturn(Optional.of(agendaEntity));
@@ -59,29 +83,22 @@ public class VoteServiceTest {
         assertEquals(AGENDA_ID, result.getAgendaId());
         assertEquals(USER_ID, result.getUserId());
         assertEquals(VOTE_ID, result.getId());
-
-
     }
+
     @Test
     public void mustGetAllVotes(){
-        var voteOne = generateVoteEntity();
-        var voteTwo = generateVoteEntity();
-        var votes = List.of(voteOne, voteTwo);
-
-        var responseOne = generateVoteResponse();
-        var responseTwo = generateVoteResponse();
-        var expectedResponses = List.of(responseOne, responseTwo);
+        var votes = List.of(voteEntity, voteEntity);
+        var expectedResponses = List.of(voteResponse, voteResponse);
 
         given(voteRepository.findAll()).willReturn(votes);
-        given(voteMapper.voteEntityIntoVoteResponse(voteOne)).willReturn(responseOne);
-        given(voteMapper.voteEntityIntoVoteResponse(voteTwo)).willReturn(responseTwo);
+        given(voteMapper.voteEntityIntoVoteResponse(voteEntity)).willReturn(voteResponse);
+        given(voteMapper.voteEntityIntoVoteResponse(voteEntity)).willReturn(voteResponse);
 
-        List<VoteResponse> result = voteService.getAllVotes();
+        var result = voteService.getAllVotes();
 
         assertEquals(expectedResponses, result);
-
-
     }
+
     @Test
     public void mustNotFindVotes(){
         given(voteRepository.findAll()).willReturn(List.of());
