4083fd3ae60626d7d2a3bfe0def99cb09ed34c84
GuiRuchell <ruchellguilherme100@gmail.com>
2025-09-04 20:46:19 -0300

    testando comando do git

--- src/main/java/br/com/student/portal/dto/student/StudentRequest.java
+++ src/main/java/br/com/student/portal/dto/student/StudentRequest.java
@@ -1,10 +1,31 @@
 package br.com.student.portal.dto.student;
 
-import lombok.Data;
-
-@Data
 public class StudentRequest {
     private String name;
     private String registration;
     private String course;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(String registration) {
+        this.registration = registration;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
 }
