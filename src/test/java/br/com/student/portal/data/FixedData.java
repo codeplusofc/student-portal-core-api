25563c9ae807030a2561bb5169e1acfb6a1cecc5
OtavioColela <otavioborges123@yahoo.com>
2025-09-05 12:14:22 -0300

    tests/creating-task-tests

--- src/test/java/br/com/student/portal/data/FixedData.java
+++ src/test/java/br/com/student/portal/data/FixedData.java
@@ -8,4 +8,5 @@ public class FixedData {
     public static final UUID VOTE_ID = UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
     public static final UUID USER_ID = UUID.fromString("321e4567-e89b-12d3-a456-426614174000");
     public static final UUID AGENDA_ID = UUID.fromString("231e4567-e89b-12d3-a456-426614174000");
+    public static final UUID TASK_ID = UUID.fromString("999e4567-e89b-12d3-a456-426614174000");
 }
