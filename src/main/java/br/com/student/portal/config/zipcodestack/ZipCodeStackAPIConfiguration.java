ab52d35b2d763f22b4ab6cdd506e1500a8f689c6
OtavioColela <otavioborges123@yahoo.com>
2025-09-15 11:23:26 -0300

    feats/starting-create-micro-service

--- src/main/java/br/com/student/portal/config/zipcodestack/ZipCodeStackAPIConfiguration.java
+++ src/main/java/br/com/student/portal/config/zipcodestack/ZipCodeStackAPIConfiguration.java
@@ -2,7 +2,9 @@ package br.com.student.portal.config.zipcodestack;
 
 import br.com.student.portal.service.ZipCodeStackAPI;
 import feign.Feign;
+import feign.codec.Encoder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -10,9 +12,17 @@ public class ZipCodeStackAPIConfiguration {
     @Value("${zipcodestack.api.url}")
     String url;
 
+    @Bean
+    public Encoder feignEncoder() {
+        return new JacksonEncoder();
+    }
+
     public ZipCodeStackAPI zipCodeStackAPI() {
+
+
+
         return Feign.builder()
-//                .decoder(new JacksonDecoder)
+                .decoder(new JacksonDecoder);
 
     }
 }
